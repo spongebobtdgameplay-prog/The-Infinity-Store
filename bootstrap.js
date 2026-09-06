eb88fb9c169d005e17d17cb5eb55d67e3b46e5d0
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-09-06 11:55:30 -0400

    Bump boot cache after readiness regression fix

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260906-v03557-no-rear-closure1";
-const Version = "0.35.57";
+const Cache = "20260906-v03558-boot-gate1";
+const Version = "0.35.58";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
